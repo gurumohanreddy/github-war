19f8526ca5cb862cd6be1c1b990219764b9b726b
gurumohanreddy <gurumohan449@gmail.com>
2017-07-11 16:41:01 +0530

    Introduce navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Popular from './Popular.js';
+import Nav from './Nav.js';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
